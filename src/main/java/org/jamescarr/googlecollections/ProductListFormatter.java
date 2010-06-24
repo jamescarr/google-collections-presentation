e9581089cc06fdd14d4328fb21f2ce8f41b35954
James Carr <james.r.carr@gmail.com>
2010-06-24 00:52:04 -0500

    refactored to Joiner

--- src/main/java/org/jamescarr/googlecollections/ProductListFormatter.java
+++ src/main/java/org/jamescarr/googlecollections/ProductListFormatter.java
@@ -3,28 +3,16 @@ package org.jamescarr.googlecollections;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.google.common.base.Joiner;
+
 public class ProductListFormatter {
 
 	public String format(List<String> products) {
-		StringBuilder builder = new StringBuilder();
-		for(String product : products){
-			if(product != null){
-				builder.append(product).append(", ");
-			}
-		}
-		return builder.delete(builder.length()-2, builder.length()).toString();
+		return Joiner.on(", ").skipNulls().join(products);
 	}
 
 	public String formatWithNulls(ArrayList<String> products) {
-		StringBuilder builder = new StringBuilder();
-		for(String product : products){
-			if(product != null){
-				builder.append(product).append(", ");
-			}else{
-				builder.append("Unknown").append(", ");
-			}
-		}
-		return builder.delete(builder.length()-2, builder.length()).toString();
+		return Joiner.on(", ").useForNull("Unknown").join(products);
 	}
 
 }
